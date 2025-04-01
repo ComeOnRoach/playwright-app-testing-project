add4cd8235ef139a12cc9632138995913842dc3c
Oleksandr Drozhcha <idroidodro@gmail.com>
2025-04-01 14:09:14 +0200

    form layourts and modal and overlays ui checks

---
+++ tests/uiComponents/form-layouts.spec.ts
@@ -0,0 +1,150 @@
+import {test, expect} from "@playwright/test"
+
+test.beforeEach(async ({page}) => {
+    await page.goto("http://localhost:4200/");
+    await page.getByRole("link", { name: 'Forms' }).click();
+    await page.getByRole("link", { name: 'Form Layout' }).click();
+})
+
+test("Inline form", async ({page}) => {
+    const inlineForm = page.locator("nb-card", { hasText: "Inline form"});
+    const name = "Oleks";
+    const email = "test.oleks@gmail.com";
+    const checkbox = inlineForm.getByText('Remember me');
+    const button = inlineForm.getByRole("button");
+    await inlineForm.getByPlaceholder("Jane Doe").fill(name);
+    await inlineForm.getByPlaceholder("Email").fill(email);
+    await checkbox.check();
+    await button.click();
+
+    const nameFieldValue = await inlineForm.getByPlaceholder("Jane Doe").inputValue();
+    const emailFieldValue = await inlineForm.getByPlaceholder("Email").inputValue();
+
+
+    expect(nameFieldValue).toEqual(name);
+    expect(emailFieldValue).toEqual(email);
+    await expect(checkbox).toBeChecked();
+    await expect(inlineForm.locator("form")).toHaveClass(/ng-submitted/);
+})
+
+test("Using the Grid", async({page}) => {
+    const usingTheGrid = page.locator('nb-card', { hasText: "Using the Grid"});
+    const email = "oleks.using.the.grid@gmail.com";
+    const password = "UsingTheGrid123";
+    const radiosOne = "Option 1";
+    const radiosTwo = "Option 2";
+    const radiosDisabledOption = "Disabled Option";
+    const button = "Sign in";
+
+    await usingTheGrid.getByRole('textbox', {name: "Email"}).fill(email);
+    await usingTheGrid.getByRole("textbox", {name: "Password"}).fill(password);
+    await usingTheGrid.getByRole("radio", {name: radiosOne}).click({force: true});
+    await usingTheGrid.getByRole("button").click();
+    
+    const getEmailValue = await usingTheGrid.getByRole("textbox", {name: "Email"}).inputValue();
+    const getPasswordValue = await usingTheGrid.getByRole("textbox", {name: "Password"}).inputValue();
+    const radioBtnOneSelected = usingTheGrid.getByRole("radio", { name: radiosOne });
+    const radioBtnTwo = await usingTheGrid.getByRole("radio", { name: radiosTwo }).isChecked();
+    const radioBtnDisabledOption = await usingTheGrid.getByRole("radio", { name: radiosDisabledOption }).isChecked();
+    const isClickedBtn = usingTheGrid.locator("form");
+
+    expect(getEmailValue).toEqual(email);
+    expect(getPasswordValue).toEqual(password);
+    await expect(radioBtnOneSelected).toBeChecked();
+    expect(radioBtnTwo).toBeFalsy();
+    expect(radioBtnDisabledOption).toBeFalsy();
+    await expect(isClickedBtn).toHaveClass("ng-untouched ng-pristine ng-valid ng-submitted");
+})
+
+test("Basic form", async ({page}) => {
+    const basicForm = page.locator("nb-card", { hasText: "Basic form"});
+    const emailAddress = "basicForm.oleks@gmail.com";
+    const password = "BasicForm123";
+
+    await basicForm.getByRole("textbox", {name: "Email"}).fill(emailAddress);
+    await basicForm.getByRole("textbox", { name: "Password" }).fill(password);
+    await basicForm.getByRole("checkbox").check({force: true});
+    await basicForm.getByRole('button').click();
+    
+    const getEmailValue = await basicForm.getByRole("textbox", { name: "Email" }).inputValue();
+    const getPasswordValue = await basicForm.getByRole("textbox", { name: "Password" }).inputValue();
+    const checkbox = basicForm.getByRole("checkbox");
+    const buttonSubmitted = basicForm.locator('form');
+    
+    expect(getEmailValue).toEqual(emailAddress);
+    expect(getPasswordValue).toEqual(password);
+    await expect(checkbox).toBeChecked();
+    await expect(buttonSubmitted).toHaveClass(/ng-submitted/);
+})
+
+test("Form without labels", async ({page}) => {
+    const formWithoutLabels = page.locator("nb-card", { hasText: "Form without labels" });
+    const recipients = "Oleks Recipient";
+    const subject = "subject without labels";
+    const message = `MindsEye, the Grand Theft Auto-esque story-driven action game led by former Rockstar Games producer Leslie Benzies, is arriving on June 10. Developed by Build A Rocket Boy Games, a new story trailer shows off the game’s setting, Redrock City, and provides a glimpse into the story and antagonists. 
+Redrock City, a fictional desert metropolis ruled by AI, consumer-grade robots, and other cutting-edge technology, is the stage for protagonist Jacob Diaz. He’s a former soldier carrying the MindsEye, a neural implant plaguing him with mysterious memories from a life-changing mission. His fight to find the truth pits him against both the government and Big Tech, namely Redrock mayor Shiva Vega, who may be a little too eager to establish complete control in her city, and Marco Silva, an eccentric tech CEO with big plans for altering the course of human evolution. Check out the trailer below. `;
+
+    await formWithoutLabels.scrollIntoViewIfNeeded();
+    await formWithoutLabels.getByRole('textbox', { name: "Recipients" }).fill(recipients);
+    await formWithoutLabels.getByRole('textbox', { name: "Subject" }).fill(subject);
+    await formWithoutLabels.getByRole('textbox', { name: "Message" }).fill(message);
+    await formWithoutLabels.getByRole('button').click();
+    
+    const recipientsInputValue = await formWithoutLabels.getByRole('textbox', { name: "Recipients" }).inputValue();
+    const subjectInputValue = await formWithoutLabels.getByRole('textbox', { name: "Subject" }).inputValue();
+    const messageInputValue = await formWithoutLabels.getByRole('textbox', { name: "Message" }).inputValue();
+    const buttonSubmitted = formWithoutLabels.locator('form');
+    
+    expect(recipientsInputValue).toEqual(recipients);
+    expect(subjectInputValue).toEqual(subject);
+    expect(messageInputValue).toEqual(message);
+    await expect(buttonSubmitted).toHaveClass(/ng-submitted/);
+})
+
+test("Block form", async({page}) => {
+    const blockForm = page.locator("nb-card", { hasText: "Block form" });
+    const firstName = "Oleks";
+    const lastName = "Dro";    
+    const email = "block.form.oleks@gmail.com";
+    const website = "https://www.linkedin.com/in/oleksandr-drozhcha-5ba665b5/";
+
+    await blockForm.getByPlaceholder("First Name").fill(firstName);
+    await blockForm.getByPlaceholder("Last Name").fill(lastName);
+    await blockForm.getByPlaceholder("Email").fill(email);
+    await blockForm.getByPlaceholder("Website").fill(website);
+    await blockForm.getByText("Submit").click();
+
+    const firstNameInputValue = await blockForm.getByPlaceholder("First Name").inputValue();
+    const lastNameInputValue = await blockForm.getByPlaceholder("Last Name").inputValue();
+    const emailInputValue = await blockForm.getByPlaceholder("Email").inputValue()
+    const websiteInputValue = await blockForm.getByRole("textbox", {name: "Website"}).inputValue();
+    const button = await blockForm.getByRole("button").textContent();
+
+    expect(firstNameInputValue).toEqual(firstName);
+    expect(lastNameInputValue).toEqual(lastName);
+    expect(emailInputValue).toEqual(email);
+    expect(websiteInputValue).toEqual(website);
+    expect(button).toEqual("Submit");
+})
+
+test("Horizontal form", async ({page}) => {
+    const horizontalForm = page.locator("nb-card", { hasText: "Horizontal form"});
+    const email = "horizontal.Form.oleks@gmail.com";
+    const password = "horizontalForm1234"
+
+    await horizontalForm.getByRole("textbox", { name: "Email" }).fill(email);
+    await horizontalForm.getByRole("textbox", { name: "Password" }).fill(password);
+    await horizontalForm.getByText("Remember me").check();
+    await horizontalForm.getByRole('button').click();
+
+    const emailInputValue = await horizontalForm.getByRole("textbox", { name: "Email" }).inputValue()
+    const passwordInputValue = await horizontalForm.getByRole("textbox", { name: "Password" }).inputValue()
+    const isCheckbox = await horizontalForm.getByRole("checkbox");
+    const buttonSubmitted = horizontalForm.locator("form");
+
+    expect(emailInputValue).toEqual(email);
+    expect(passwordInputValue).toEqual(password);
+    await expect(isCheckbox).toBeChecked();
+    await expect(buttonSubmitted).toHaveClass(/ng-submitted/);
+})
+
---
+++ tests/uiComponents/modal-and-overlays.spec.ts
@@ -0,0 +1,252 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:4200");
+    await page.getByRole('link', { name: "Modal & Overlays" }).click();
+    await page.getByRole('link', { name: "Toastr" }).click();
+})
+
+
+test.describe("Modal & Overlays => Toastr => Toaster configuration => Position button", () => {
+
+    test("Toaster configuration => Position button => Top Left", async ({ page }) => {
+        const topLeft = "top-left";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const topLeftBtnLocator = page.locator("nb-option").getByText(topLeft);
+
+        await positionBtn.click();
+        await topLeftBtnLocator.click();
+        await expect(positionBtn).toHaveText(topLeft);
+    });
+
+    test("Toaster configuration => Position button => Bottom Left", async ({ page }) => {
+        const bottomLeft = "bottom-left";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const bottomLeftBtnLocator = page.locator("nb-option").getByText(bottomLeft);
+
+        await positionBtn.click();
+        await bottomLeftBtnLocator.click();
+        await expect(positionBtn).toHaveText(bottomLeft);
+    });
+
+    test("Toaster configuration => Position button => Bottom Right", async ({ page }) => {
+        const bottomRight = "bottom-right";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const bottomRightBtnLocator = page.locator("nb-option").getByText(bottomRight);
+
+        await positionBtn.click();
+        await bottomRightBtnLocator.click();
+        await expect(positionBtn).toHaveText(bottomRight);
+    });
+
+    test("Toaster configuration => Position button => Top End", async ({ page }) => {
+        const topEnd = "top-end";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const topEndBtnLocator = page.locator("nb-option").getByText(topEnd);
+
+        await positionBtn.click();
+        await topEndBtnLocator.click();
+        await expect(positionBtn).toHaveText(topEnd);
+    });
+
+    test("Toaster configuration => Position button => Top Start", async ({ page }) => {
+        const topStart = "top-start";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const topStartBtnLocator = page.locator("nb-option").getByText(topStart);
+
+        await positionBtn.click();
+        await topStartBtnLocator.click();
+        await expect(positionBtn).toHaveText(topStart);
+    });
+
+    test("Toaster configuration => Position button => Bottom End", async ({ page }) => {
+        const bottomEnd = "bottom-end";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const bottomEndBtnLocator = page.locator("nb-option").getByText(bottomEnd);
+
+        await positionBtn.click();
+        await bottomEndBtnLocator.click();
+        await expect(positionBtn).toHaveText(bottomEnd);
+    });
+
+    test("Toaster configuration => Position button => Bottom Start", async ({ page }) => {
+        const bottomStart = "bottom-start";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const bottomStartBtnLocator = page.locator("nb-option").getByText(bottomStart);
+
+        await positionBtn.click();
+        await bottomStartBtnLocator.click();
+        await expect(positionBtn).toHaveText(bottomStart);
+    });
+
+    test("Toaster configuration => Position button => Top Right", async ({ page }) => {
+        const topRight = "top-right";
+        const positionBtn = page.locator(".form-group", { hasText: "Position:" }).getByRole("button");
+        const topRightBtnLocator = page.locator("nb-option").getByText(topRight);
+
+        await positionBtn.click();
+        await topRightBtnLocator.click();
+        await expect(positionBtn).toHaveText(topRight);
+    });
+})
+
+test.describe("Modal & Overlays => Toastr => Toaster configuration => Toast Type button", () => {
+
+    test("Modal & Overlays => Toastr => Toaster configuration => Toast Type Value", async({page}) => {
+        const toastTypeName = "Toast type:"
+        const toastTypeValue = page.locator( "label", {hasText: "Toast type:"});
+        await expect(toastTypeValue).toHaveText(toastTypeName);
+    })
+
+    test("Toaster configuration => Toast Type button => Success", async ({ page }) => {
+        const successBtn = "success";
+        const btnLocation = page.locator(".form-group", { hasText: "Toast type"}).getByRole("button");
+        const menuOption = page.locator("nb-option", { hasText: successBtn });
+
+        await btnLocation.click();
+        await menuOption.click();
+        await expect(btnLocation).toHaveText(successBtn);
+    });
+
+    test("Toaster configuration => Toast Type button => Info", async ({ page }) => {
+        const infoBtn = "info";
+        const btnLocation = page.locator(".form-group", { hasText: "Toast type" }).getByRole("button");
+        const menuOption = page.locator("nb-option", { hasText: infoBtn });
+
+        await btnLocation.click();
+        await menuOption.click();
+        await expect(btnLocation).toHaveText(infoBtn);
+    });
+
+    test("Toaster configuration => Toast Type button => Warning", async ({ page }) => {
+        const warningBtn = "warning";
+        const btnLocation = page.locator(".form-group", { hasText: "Toast type" }).getByRole("button");
+        const menuOption = page.locator("nb-option", { hasText: warningBtn });
+
+        await btnLocation.click();
+        await menuOption.click();
+        await expect(btnLocation).toHaveText(warningBtn);
+    });
+
+    test("Toaster configuration => Toast Type button => Danger", async ({ page }) => {
+        const dangerBtn = "danger";
+        const btnLocation = page.locator(".form-group", { hasText: "Toast type" }).getByRole("button");
+        const menuOption = page.locator("nb-option", { hasText: dangerBtn });
+
+        await btnLocation.click();
+        await menuOption.click();
+        await expect(btnLocation).toHaveText(dangerBtn);
+    });
+
+    test("Toaster configuration => Toast Type button => Primary", async ({ page }) => {
+        const primaryBtn = "primary";
+        const dangerBtn = "danger";
+        const btnLocation = page.locator(".form-group", { hasText: "Toast type" }).getByRole("button");
+        const menuOptionDanger = page.locator("nb-option", { hasText: dangerBtn });
+        const menuOptionPrimary = page.locator("nb-option", { hasText: primaryBtn });
+
+        await btnLocation.click();
+        await menuOptionDanger.click();
+        await btnLocation.click();
+        await menuOptionPrimary.click();
+        await expect(btnLocation).toHaveText(primaryBtn);
+    });
+
+})
+
+test.describe("Modal & Overlays => Toastr => Toaster configuration => 'Title', 'Content', 'Time to hide toast' fields", ()=>{
+    test("Title field", async ({page}) => {
+        const emptyInputValue = "";
+        const filledInputValue = "oleks.test@$#@$$@$Test";
+        const titleField = page.locator(".form-group", {hasText: "Title"});
+
+        await titleField.getByRole('textbox').clear();
+        let titleInputValue = await titleField.getByRole('textbox').inputValue();
+        expect(titleInputValue).toEqual(emptyInputValue);
+
+        await titleField.getByRole('textbox').fill(filledInputValue);
+        titleInputValue = await titleField.getByRole('textbox').inputValue();
+        expect(titleInputValue).toEqual(filledInputValue);
+
+    }) 
+
+    test("Content field", async ({ page }) => {
+        const emptyInputValue = "";
+        const filledInputValue = "Cool test QA :)";
+        const titleField = page.locator(".form-group", { hasText: "Content" });
+
+        await titleField.getByRole('textbox').clear();
+        let titleInputValue = await titleField.getByRole('textbox').inputValue();
+        expect(titleInputValue).toEqual(emptyInputValue);
+
+        await titleField.getByRole('textbox').fill(filledInputValue);
+        titleInputValue = await titleField.getByRole('textbox').inputValue();
+        expect(titleInputValue).toEqual(filledInputValue);
+
+    }) 
+
+    test("Time to hide toast", async ({ page }) => {
+        const emptyInputValue = "";
+        const toastTimeDelay = "15000";
+        const fieldName = "Time to hide toast, ms. 0 to persistent toast:";
+        const titleField = page.locator(".form-group", { hasText: "Time to hide toast" });
+
+        await expect(titleField).toHaveText(fieldName);
+        await titleField.getByRole('spinbutton').clear();
+        let titleInputValue = await titleField.getByRole('spinbutton').inputValue();
+        expect(titleInputValue).toEqual(emptyInputValue);
+
+        await titleField.getByRole('spinbutton').fill(toastTimeDelay);
+        titleInputValue = await titleField.getByRole('spinbutton').inputValue();
+        expect(titleInputValue).toEqual(toastTimeDelay);
+
+    }) 
+
+})
+
+test.describe("Modal & Overlays => Toastr => Toaster configuration => Checkboxes", () => {
+
+    test("Modal & Overlays => Toastr => Toaster configuration => Hide on click - unchecked", async ({page}) => {
+        const hideOnClickCheckbox = page.getByRole("checkbox", { name: "Hide on click"});
+
+        await hideOnClickCheckbox.uncheck({force: true});
+        await expect(hideOnClickCheckbox).not.toBeChecked();
+    })
+
+    test("Modal & Overlays => Toastr => Toaster configuration => Hide on click - checked", async ({ page }) => {
+        const hideOnClickCheckbox = page.getByRole("checkbox", { name: "Hide on click" });
+
+        await hideOnClickCheckbox.check({ force: true });
+        await expect(hideOnClickCheckbox).toBeChecked();
+    })
+
+    test("Modal & Overlays => Toastr => Toaster configuration => Prevent arising of duplicate toast - unchecked", async ({ page }) => {
+        const preventArisingOfDuplicate = page.getByRole("checkbox", { name: "Prevent arising of duplicate toast" });
+
+        await preventArisingOfDuplicate.uncheck({ force: true });
+        await expect(preventArisingOfDuplicate).not.toBeChecked();
+    })
+
+    test("Modal & Overlays => Toastr => Toaster configuration => Prevent arising of duplicate toast - checked", async ({ page }) => {
+        const preventArisingOfDuplicate = page.getByRole("checkbox", { name: "Prevent arising of duplicate toast" });
+
+        await preventArisingOfDuplicate.check({ force: true });
+        await expect(preventArisingOfDuplicate).toBeChecked();
+    })
+
+    test("Modal & Overlays => Toastr => Toaster configuration => Show toast with icon - unchecked", async ({ page }) => {
+        const showToastWithIcon = page.getByRole("checkbox", { name: "Show toast with icon" });
+
+        await showToastWithIcon.uncheck({ force: true });
+        await expect(showToastWithIcon).not.toBeChecked();
+    })
+
+    test("Modal & Overlays => Toastr => Toaster configuration => Show toast with icon - checked", async ({ page }) => {
+        const showToastWithIcon = page.getByRole("checkbox", { name: "Show toast with icon" });
+
+        await showToastWithIcon.check({ force: true });
+        await expect(showToastWithIcon).toBeChecked();
+    })
+
+
+})
